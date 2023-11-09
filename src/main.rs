629df3b35508b67672dfd80e5c30c6daf088357b
Patrick José Pereira <patrickelectric@gmail.com>
2023-11-09 06:52:31 -0300

    main: Add web server
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod types;
 mod server;
 
-fn main() {
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
     println!("Hello, world!");
+    server::manager::run("0.0.0.0:8080").await
 }
