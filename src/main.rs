36525810331aa7abd36284fc698c6cd2d926184b
Patrick José Pereira <patrickelectric@gmail.com>
2023-11-10 14:03:37 -0300

    Add logger
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod server;
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
+    std::env::set_var("RUST_LOG", "trace");
+    env_logger::init();
+
     println!("Hello, world!");
     server::manager::run("0.0.0.0:8080").await
 }
